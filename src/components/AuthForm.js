102cdf230992162c7a41edef9ceac63b5622e2b9
Travis Neufeld <tdizzletrav@gmail.com>
2023-02-03 20:11:00 -0700

    Auth error messaging and routing

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -3,96 +3,103 @@ import { LockOutlined, UserOutlined } from "@ant-design/icons";
 import { Button, Checkbox, Form, Input } from "antd";
 import "./AuthForm.css";
 
-export function AuthForm({ onFinish, isSignUp }) {
+export function AuthForm({ onFinish, isSignUp, errorMessage }) {
   return (
-    <Form
-      name="normal_login"
-      className="login-form"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-    >
-      <Form.Item
-        name="email"
-        rules={[
-          {
-            type: "email",
-            message: "Please enter a valid email address",
-          },
-          {
-            required: true,
-            message: "Please input your Email",
-          },
-        ]}
+    <div className="auth-form-container">
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
       >
-        <Input
-          prefix={<UserOutlined className="site-form-item-icon" />}
-          placeholder="Email"
-        />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: "Please input your Password",
-          },
-        ]}
-      >
-        <Input
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          type="password"
-          placeholder="Password"
-        />
-      </Form.Item>
-      {isSignUp ? (
         <Form.Item
-          name="confirm"
-          dependencies={["password"]}
-          hasFeedback
+          name="email"
+          rules={[
+            {
+              type: "email",
+              message: "Please enter a valid email address",
+            },
+            {
+              required: true,
+              message: "Please input your Email",
+            },
+          ]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Email"
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
           rules={[
             {
               required: true,
-              message: "Please confirm your password",
+              message: "Please input your Password",
             },
-            ({ getFieldValue }) => ({
-              validator(_, value) {
-                if (!value || getFieldValue("password") === value) {
-                  return Promise.resolve();
-                }
-                return Promise.reject(new Error("Passwords do not match"));
-              },
-            }),
           ]}
         >
           <Input
             prefix={<LockOutlined className="site-form-item-icon" />}
             type="password"
-            placeholder="Confirm Password"
+            placeholder="Password"
           />
         </Form.Item>
-      ) : (
-        <Form.Item>
-          <Form.Item name="remember" valuePropName="checked" noStyle>
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
-
-          <a className="login-form-forgot" href="/">
-            Forgot password
-          </a>
-        </Form.Item>
-      )}
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button">
-          {isSignUp ? "Sign up" : "Log in"}
-        </Button>
         {isSignUp ? (
-          <Link to={`/login`}>Already have an account?</Link>
+          <Form.Item
+            name="confirm"
+            dependencies={["password"]}
+            hasFeedback
+            rules={[
+              {
+                required: true,
+                message: "Please confirm your password",
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue("password") === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(new Error("Passwords do not match"));
+                },
+              }),
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Confirm Password"
+            />
+          </Form.Item>
         ) : (
-          <Link to={`/signup`}>register now!</Link>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <a className="login-form-forgot" href="/">
+              Forgot password
+            </a>
+          </Form.Item>
         )}
-      </Form.Item>
-    </Form>
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            {isSignUp ? "Sign up" : "Log in"}
+          </Button>
+          {isSignUp ? (
+            <Link to={`/login`}>Already have an account?</Link>
+          ) : (
+            <Link to={`/signup`}>register now!</Link>
+          )}
+        </Form.Item>
+      </Form>
+      <div className="auth-form-error-text">{errorMessage}</div>
+    </div>
   );
 }
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,5 @@
-import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { AuthForm } from "../components/AuthForm";
 import "./Auth.css";
 import {
@@ -11,6 +12,9 @@ const auth = getAuth();
 
 export default function Auth() {
   const { pathname } = useLocation();
+  let navigate = useNavigate();
+  const [errorMessage, setErrorMessage] = useState(null);
+
   const isSignUp = pathname === "/signup";
 
   const onFinish = (values) => {
@@ -21,11 +25,15 @@ export default function Auth() {
           // Account created
           const user = userCredential.user;
           console.log(user);
+          navigate("/");
         })
         .catch((error) => {
           const errorCode = error.code;
           const errorMessage = error.message;
           console.log(`A ${errorCode} error has occurred: ${errorMessage}`);
+          setErrorMessage(
+            "An error occurred while creating your account. Please try again."
+          );
         });
     } else {
       signInWithEmailAndPassword(auth, email, password)
@@ -33,11 +41,15 @@ export default function Auth() {
           // Signed in
           const user = userCredential.user;
           console.log(user);
+          navigate("/");
         })
         .catch((error) => {
           const errorCode = error.code;
           const errorMessage = error.message;
           console.log(`A ${errorCode} error has occurred: ${errorMessage}`);
+          setErrorMessage(
+            "An error occurred while signing you in. Please try again."
+          );
         });
       // log in
     }
@@ -45,7 +57,11 @@ export default function Auth() {
 
   return (
     <div className="auth-page-container">
-      <AuthForm onFinish={onFinish} isSignUp={isSignUp} />
+      <AuthForm
+        onFinish={onFinish}
+        isSignUp={isSignUp}
+        errorMessage={errorMessage}
+      />
     </div>
   );
 }
