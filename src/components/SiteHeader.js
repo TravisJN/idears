ebd8c4b1b9e799fb4e4cbdf64207321cde3a869d
Travis Neufeld <tdizzletrav@gmail.com>
2023-02-25 14:21:37 -0700

    Add background, styling updates

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -1,18 +1,11 @@
 import "./SiteHeader.css";
-import { BulbTwoTone } from "@ant-design/icons";
 import User from "./User";
 
 export function SiteHeader({ user }) {
   return (
     <div className="header-container">
       <div className="header-title-container">
-        <BulbTwoTone
-          twoToneColor="white"
-          rotate={-15}
-          className="header-icon"
-        />
-        <p className="header-title">outofinsight</p>
-        <BulbTwoTone twoToneColor="white" rotate={15} className="header-icon" />
+        <p className="header-title">OutofinsighT</p>
       </div>
       <User user={user} />
     </div>
